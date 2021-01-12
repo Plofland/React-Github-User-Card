ce1d0ed1dbb3adbb8b24f05473c3869080084cc2
Plofland <ploflan@gmail.com>
2021-01-12 18:57:50 -0500

    Still having issues just getting a for loop to set state

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -6,6 +6,14 @@ const Heading = styled.div``;
 
 const GithubCards = styled.div``;
 
+const SingleCard = styled.div``;
+
+const Avatar = styled.img``;
+
+const ProfileInfo = styled.div``;
+
+const UserInfo = styled.div``;
+
 const teamMembers = [
   { name: 'April Ashby', login: 'aprilissy' },
   { name: 'Emily Ryan', login: 'emilyr027' },
@@ -17,33 +25,72 @@ const teamMembers = [
 export default class App extends Component {
   state = {
     teamMembers
+    // name: ''
   };
 
   componentDidMount() {
-    const devInfo = async () => {
-      const developers = [...teamMembers];
-      for (let i in teamMembers) {
-        const { data } = await axios.get(
-          `https://api.github.com/users/${team[i].login}`
-        );
-        developers[i].image = data.avatar_url;
-        developers[i].bio = data.bio;
-        developers[i].location = data.location;
-        developers[i].url = data.html_url;
-        developers[i].followers = data.followers;
-        developers[i].following = data.following;
-      }
-      this.setState(developers);
-    };
-    devInfo();
+    const developers = [...teamMembers];
+
+    for (let i in teamMembers) {
+      axios
+        .get(`https://api.github.com/users/${teamMembers[i].login}`)
+        .then((res) => {
+          console.log(res);
+
+          this.state({
+            image: res.data.avatar_url,
+            bio: res.data.bio,
+            location: res.data.location,
+            url: res.data.html_url,
+            followers: res.data.followers,
+            following: res.data.following
+          });
+          // developers[i].image = res.data.avatar_url,
+          // developers[i].bio = res.data.bio,
+          // developers[i].location = res.data.location,
+          // developers[i].url = res.data.html_url,
+          // developers[i].followers = res.data.followers,
+          // developers[i].following = res.data.following,
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      // this.setState(developers);
+    }
+
+    // const devInfo = async () => {
+    //   const developers = [...teamMembers];
+    //   for (let i in teamMembers) {
+    //     const { data } = await axios.get(
+    //       `https://api.github.com/users/${teamMembers[i].login}`
+    //     );
+    //     console.log(data);
+    //     developers[i].image = data.avatar_url;
+    //     developers[i].bio = data.bio;
+    //     developers[i].location = data.location;
+    //     developers[i].url = data.html_url;
+    //     developers[i].followers = data.followers;
+    //     developers[i].following = data.following;
+    //   }
+    //   this.setState({ developers });
+    // };
+    // devInfo();
   }
 
   render() {
     return (
       <>
         <Heading>Github Track Team</Heading>
-        <GithubCards>
-          {teamMembers.map((dev) => {
+        <p>{this.state.name}</p>
+        <img src={this.state.image} alt="profile avatar" />
+        <p>Bio: {this.state.bio}</p>
+        <p>Username: {this.state.login}</p>
+        <p>Location: {this.state.location}</p>
+        <a href={this.state.url}>GitHub Profile</a>
+        <p>Followers: {this.state.followers}</p>
+        <p>Followering: {this.state.following}</p>
+        {/* <GithubCards>
+          {this.teamMembers.map((dev) => {
             return (
               <SingleCard>
                 <Avatar src={dev.image} alt="GitHub Profile Picture" />
@@ -61,7 +108,7 @@ export default class App extends Component {
               </SingleCard>
             );
           })}
-        </GithubCards>
+        </GithubCards> */}
       </>
     );
   }
